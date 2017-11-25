2848d4d2d25418e894515d9ff334213044151d80
MaddoxSt <subittimalsina@gmail.com>
2017-11-25 18:46:20 +0545

    Added Scripts of character controller and camera controller
    Added the basic character controller and the camera controller  with player animations and camera follow the player and face to the camera side

---
+++ Scripts/Camera Script/followCam.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class followCam : MonoBehaviour {
+
+	float yaw;
+	float pitch;
+
+	public Transform target;
+	public float distanceFromTarget = 2f;
+
+	public Transform towardsPlayerMove;
+
+	public float mouseSensitivity = 5f;
+
+	public Vector2 maxMin = new Vector2(-5,30);
+
+	public float rotationSmooth = 0.2f;
+
+	Vector3 rotationSmoothVelocity;
+	Vector3 currentRotation;
+
+
+	void Start(){
+
+		towardsPlayerMove.transform.position = target.transform.position;
+		towardsPlayerMove.transform.parent = target.transform;
+		Cursor.lockState=CursorLockMode.Locked;
+		
+		
+
+
+	}
+	void LateUpdate(){
+
+		yaw += Input.GetAxis ("Mouse X")*mouseSensitivity;
+		pitch-=Input.GetAxis("Mouse Y")* mouseSensitivity;
+		pitch = Mathf.Clamp (pitch, maxMin.x, maxMin.y);
+
+		currentRotation = Vector3.SmoothDamp (currentRotation, new Vector3 (pitch, yaw), ref rotationSmoothVelocity, rotationSmooth);
+
+		transform.eulerAngles = currentRotation;
+
+		transform.position = target.position - transform.forward * distanceFromTarget;
+	}
+}
---
+++ Scripts/Player Scripts/CharMove.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using UnityEngine;
+using System.Collections;
+
+public class CharMove : MonoBehaviour {
+
+
+	//For walk and run speed
+	public float walkSpeed = 2;
+	public float runSpeed = 6;
+
+	//Walk and turn smothing
+	public float turnSmoothTime = 0.2f;
+	float turnSmoothVelocity;
+	public float speedSmoothTime = 0.1f;
+	float speedSmoothVelocity;
+	float currentSpeed;
+
+	//Camera's transform
+	Transform cameraMovement;
+
+
+	//Prevent attack spamming
+	[SerializeField]
+	float attackReset = 0.5f;
+	bool canAttack = true;
+
+	//Setting the gravity and velocity
+	public float gravity=-10f;
+	float velocityY;
+
+	Animator animator;
+	CharacterController controller;
+
+
+	void Start () {
+		animator = GetComponent<Animator>();
+		controller = GetComponent<CharacterController> ();
+		cameraMovement = Camera.main.transform;
+
+	}
+
+	void Update () {
+
+		Attack ();
+		//Walking and Running
+		Vector2 input = new Vector2 (Input.GetAxisRaw ("Horizontal"), Input.GetAxisRaw ("Vertical"));
+		Vector2 inputDir = input.normalized;
+
+
+
+		if (inputDir != Vector2.zero) {
+			float targetRotation = Mathf.Atan2 (inputDir.x, inputDir.y) * Mathf.Rad2Deg + cameraMovement.eulerAngles.y;
+			transform.eulerAngles = Vector3.up * Mathf.SmoothDampAngle(transform.eulerAngles.y, targetRotation, ref turnSmoothVelocity, turnSmoothTime);
+		}
+
+		bool running = Input.GetKey (KeyCode.LeftShift);
+		float targetSpeed = ((running) ? runSpeed : walkSpeed) * inputDir.magnitude;
+		currentSpeed = Mathf.SmoothDamp (currentSpeed, targetSpeed, ref speedSmoothVelocity, speedSmoothTime);
+
+		velocityY += Time.deltaTime * gravity;
+
+		Vector3 velocity = transform.forward * currentSpeed +Vector3.up*velocityY;
+		if (controller.isGrounded) {
+			velocityY = 0;
+		}
+		controller.Move (velocity * Time.deltaTime);
+
+		float animationSpeedPercent = ((running) ? 1 : .5f) * inputDir.magnitude;
+		animator.SetFloat ("speedPercent", animationSpeedPercent, speedSmoothTime, Time.deltaTime);
+	}
+
+
+	//Method responsible for attack animations and attack spam check
+
+	void Attack(){
+
+		if (Input.GetKeyDown(KeyCode.Mouse0) && canAttack) {
+			canAttack = false;
+			animator.SetTrigger ("attack1");
+			Invoke ("ResetAttackTimer", attackReset);
+		}
+
+		if (Input.GetKeyDown(KeyCode.Mouse1) && canAttack) {
+			canAttack = false;
+			animator.SetTrigger ("attack2");
+			Invoke ("ResetAttackTimer", attackReset);
+		}
+			
+	}
+
+
+	//Method to set the attacking to true 
+	void ResetAttackTimer(){
+		canAttack = true;
+	}
+
+}
+
